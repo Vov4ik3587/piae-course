4e8dcca1a8dea2c8a19342cbe25a0cdcd677ce77
Vov4ik3587 <galstyan.vova@gmail.com>
2023-03-25 14:20:07 +0700

    ну почти

--- GUI/boyan.py
+++ GUI/boyan.py
@@ -1,25 +1,5 @@
-from tkinter import *
-from tkinter.ttk import *
 import numpy as np
 import matplotlib.pyplot as plt
-
-plt.rcParams['font.family'] = 'fantasy'
-plt.rcParams['font.fantasy'] = 'Arial'
-
-
-def inserterErorr(value):
-    output.delete("1.0", "end")
-    output.insert("1.0", value)
-
-
-def inserter(value):
-    output.insert("1.0", value)
-
-
-def clear(event):
-    """ Clears entry form """
-    caller = event.widget
-    caller.delete("0", "end")
 
 
 # заполняем входной файл
@@ -35,10 +15,10 @@ def write(filename, x, p):
 
 
 # создание матрицы информационной матрицы М
-def makeM(x, p, linear):
-    M = np.zeros((len(func(x[0], linear)), len(func(x[0], linear))))
+def makeM(x, p, delta):
+    M = np.zeros((len(func(x[0], delta)), len(func(x[0], delta))))
     for i in range(len(x)):
-        M += p[i] * make_partM(func(x[i], linear))
+        M += p[i] * make_partM(func(x[i], delta))
     return M
 
 
@@ -52,29 +32,17 @@ def make_partM(fx):
 
 
 # возвращает вектор со значениями нашей функции
-def func(x, linear):
-    if linear:
-        return np.array([mu1(x), x * mu1(x), mu2(x), x * mu2(x)])
-    else:
-        return np.array([mu1(x), x * mu1(x), x ** 2 * mu1(x), mu2(x), x * mu2(x), x ** 2 * mu2(x)])
+def func(x, delta):
+    return np.array([1, x, mu1(x, delta), x * mu1(x, delta)])
 
 
-def mu1(dot):
-    if -0.5 > dot >= -1.0:
-        return 1.0
-    elif 0.5 >= dot >= -0.5:
-        return 1 / 2 - dot
-    elif 1.0 >= dot > 0.5:
+def mu1(dot, delta):
+    if -delta >= dot >= -1:
+        return 1.
+    elif delta >= dot >= -delta:
+        return (delta - dot) / 2 * delta
+    elif dot > delta:
         return 0
-
-
-def mu2(dot):
-    if -0.5 > dot >= -1.0:
-        return 0
-    elif 0.5 >= dot >= -0.5:
-        return 1 / 2 + dot
-    elif 1.0 >= dot > 0.5:
-        return 1
 
 
 # создание начального плана
@@ -88,33 +56,31 @@ def makeD(M):
 
 
 # Добавление новой точки
-def addNewPoint(x, p, grid, linear, flag, usereps):
-    M = makeM(x, p, linear)
+def addNewPoint(x, p, grid, delta, flag, usereps=0.01):
+    M = makeM(x, p, delta)
     D = makeD(M)
-    curientD = D_optim(M)
-    max, maxdot = findMaxFi(grid, D, linear)
+    curientD = A_optim(D)
+    max, maxdot = findMaxFi(grid, D, delta)
     if flag:
         x.append(maxdot)
-        p = addToP(curientD, p, x, linear)
+        p = addToP(curientD, p, x, delta)
     eps = max * usereps
-    delta = abs(max - np.trace(np.dot(M, D)))
-    # np.around((np.linalg.det(M)), 9))
-    inserter("%04.14e \n" % np.trace(M))
+    delta = abs(max - np.trace(D @ D @ M))
     print(np.linalg.det(M))
     return delta, eps, x, p
 
 
 # Проверка на D-оптимальность ( вычисление критерия для заданной матрицы)
-def D_optim(M):
-    return np.linalg.det(M)
+def A_optim(D):
+    return -np.trace(D)
 
 
 # функция нахождения максимума на сетке, для добавления новой точки
-def findMaxFi(grid, D, linear):
-    max = np.dot(np.dot(func(grid[0], linear), D**2), func(grid[0], linear).T)
+def findMaxFi(grid, D, delta):
+    max = np.dot(np.dot(func(grid[0], delta), D @ D), func(grid[0], delta).T)
     maxdot = grid[0]
     for i in grid:
-        f = np.dot(np.dot(func(i, linear), D**2), func(i, linear).T)
+        f = np.dot(np.dot(func(i, delta), D @ D), func(i, delta).T)
         if f > max:
             max = f
             maxdot = i
@@ -122,7 +88,7 @@ def findMaxFi(grid, D, linear):
 
 
 # Подбор веса для новой точки
-def addToP(curientD, p, x, linear):
+def addToP(curientD, p, x, delta):
     newD = curientD - 1
     ksy = 1
     while curientD > newD:
@@ -130,8 +96,9 @@ def addToP(curientD, p, x, linear):
         for i in range(len(newP)):
             newP[i] = (1.0 - ksy / len(newP)) * newP[i]
         newP.append(ksy / len(newP))
-        newM = makeM(x, newP, linear)
-        newD = D_optim(newM)
+        newM = makeM(x, newP, delta)
+        newD = makeD(newM)
+        newD = A_optim(newD)
         ksy /= 2
     return newP
 
@@ -178,9 +145,9 @@ def removeDotsWithSmallWeitgh(x, p):
     return x, p
 
 
-def Plot(x, grid, D, label, linear):
+def Plot(x, grid, D, label, delta):
     Dfunc = list(map(lambda x: np.dot(
-        np.dot(func(x, linear), D**2), func(x, linear)), grid))
+        np.dot(func(x, delta), D**2), func(x, delta)), grid))
     maxD = max(Dfunc)
     for i in x:
         plt.scatter(i, 0)
@@ -188,49 +155,37 @@ def Plot(x, grid, D, label, linear):
         plt.plot([i, i], [0, maxD], color='grey')
     plt.plot([x[0], x[0]], [0, maxD], color='grey', label='План')
     plt.plot(grid, Dfunc, color='red', label='d(x,e)')
-    F = [mu1, mu2]
+    F = [mu1]
     Color = ['black', 'green']
     Type = ['--', '.-']
     for f, t, c in zip(F, Type, Color):
-        pltmu = list(map(lambda x: f(x), grid))
+        pltmu = list(map(lambda x: f(x, delta), grid))
         plt.plot(grid, pltmu, t, color=c, label="{i}".format(i=f.__name__))
     plt.legend(loc='best')
-    plt.title(label, family="verdana")
     plt.xlabel('x')
     plt.ylabel('f(x)')
     plt.show()
 
 
 def main():
-    output.delete("1.0", "end")
-    try:
-        e_val = int(e.get())
-    except ValueError:
-        inserterErorr("Количество элементов должно быть целым!")
-    try:
-        r_val = float(r.get())
-    except ValueError:
-        inserterErorr("Формат ввода точности: 0.1 \n Повторите ввод!")
-    if combobox.current() == 0:
-        linear = True
-    else:
-        linear = False
-
-    x, p = makeUPlan(e_val)
-
-    write("input.txt", x, p)
-    D = makeD(makeM(x, p, linear))
-    grid = np.linspace(-1, 1, 101)
-    Plot(x, grid, D, "График начального плана", linear)
+    Delta = 0.5
+    N = 20
     delta = 1
     eps = 0.001
     iteration = 1
     sumiteration = 0
     ito = 0
+
+    x, p = makeUPlan(N)
+    write("input.txt", x, p)
+    D = makeD(makeM(x, p, Delta))
+    grid = np.linspace(-1, 1, 101)
+    Plot(x, grid, D, "График начального плана", Delta)
+
     while abs(delta) > eps:
         smalliteration = 0
         while delta > eps:
-            delta, eps, x, p = addNewPoint(x, p, grid, linear, True, r_val)
+            delta, eps, x, p = addNewPoint(x, p, grid, Delta, True)
             smalliteration += 1
             if smalliteration > 10000:
                 break
@@ -238,10 +193,8 @@ def main():
 
         x, p = removeDotsWithSmallWeitgh(x, p)
         ito += 1
-        inserter("\nКоличество добавленных точек: %s \n\n" % smalliteration)
         sumiteration += smalliteration
-        inserter("\nОчистка № %s \n" % ito)
-        delta, eps, x, p = addNewPoint(x, p, grid, linear, False, r_val)
+        delta, eps, x, p = addNewPoint(x, p, grid, Delta, False)
 
         iteration += 1
         print(iteration)
@@ -249,68 +202,9 @@ def main():
             break
 
     write("output.txt ", x, p)
-    D = makeD(makeM(x, p, linear))
-    Plot(x, grid, D, "График оптимального плана", linear)
-    inserter("Оптимальный план построен за %s итераций \n" % sumiteration)
-    allw = output.get('1.0', END)
-    output.delete("1.0", "end")
-    inserter("%s " % revers(allw))
+    D = makeD(makeM(x, p, Delta))
+    Plot(x, grid, D, 'График оптимального плана', Delta)
 
 
-def revers(s):
-    s = s.split("\n")
-    s.reverse()
-    a = "Результаты: \n\nОпределитель матрицы M:"
-    for i in s:
-        a += i + '\n'
-    return a
-
-
-root = Tk()
-# установить расположения окна по центру
-x = (root.winfo_screenwidth() - root.winfo_reqwidth()) / 8
-y = (root.winfo_screenheight() - root.winfo_reqheight()) / 8
-root.wm_geometry("+%d+%d" % (x, y))
-root.title("Синтез непрерывных D-планов эксперимента для нечетких однофакторных моделей с двумя подобластями определения")
-root.minsize(900, 600)
-root.resizable(width=False, height=False)
-
-frame = Frame(root)
-frame.grid()
-
-# выбор модели
-lab_mod = Label(frame, text="Выберите модель:").grid(
-    column=1, row=0, pady=(10, 0), padx=(30, 0))
-combobox = Combobox(frame, values=[u"Линейная", u"Квадратичная"], height=2)
-combobox.set(u"Линейная")
-combobox.grid(column=2, row=0, pady=(10, 0), padx=(5, 0))
-
-# точность
-lab_t = Label(frame, text="Введите точность:").grid(
-    column=3, row=0, pady=(10, 0), padx=(30, 0))
-vt = StringVar()
-vt.set('0.01')
-r = Entry(frame, width=23, textvariable=vt)
-r.grid(row=0, column=4, padx=(5, 0), pady=(10, 0))
-r.bind("<FocusIn>", clear)
-
-# количество
-lab_n = Label(frame, text="Введите количество элементов:").grid(
-    column=5, row=0, padx=(30, 0), pady=(10, 0))
-v = StringVar()
-v.set('25')
-e = Entry(frame, width=23, textvariable=v)
-e.grid(row=0, column=6, padx=(5, 0), pady=(10, 0))
-e.bind("<FocusIn>", clear)
-
-but = Button(frame, text="Решить", command=main)
-but.grid(row=0, column=7, padx=(30, 0), pady=(10, 0))
-
-output = Text(frame, bg="white", font="Arial 12", width=100, height=30)
-output.grid(row=1, column=1, columnspan=7, rowspan=4, pady=(10, 0))
-
-scrollbar = Scrollbar(command=output.yview, orient=VERTICAL)
-scrollbar.grid(row=0, column=1, sticky='ns', padx=(30, 0))
-output.configure(yscrollcommand=scrollbar.set)
-
-root.mainloop()
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -1,30 +1,147 @@
-from tkinter import Tk
-from tkinter import Label
-from tkinter import ttk
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.use("TKAgg")
+
+# Линейная однофакторная модель
 
 
 
-if __name__  == '__main__':
- 
-    root = Tk()     # создаем корневой объект - окно
-    root.title("Курсовая работа ПиАЭ")     # устанавливаем заголовок окна
-    root.geometry("1280x960")    # устанавливаем размеры окна
+
+
+def model(x, delta):
+    return [[1], [x], [mu1(x, delta)], [mu1(x, delta) * x]]
+
+# Вычисление информационных матриц для каждого плана
+
+
+def inf_matrix(x, p, delta):
+    M = np.zeros((6, 6))
+    for i in range(len(x)):
+        M += p[i] * np.dot(model(x[i], delta),
+                           np.transpose(model(x[i], delta)))
+    return M
+
+
+# Вычисление дисперсионной матрицы
+
+def disp_matrix(M):
+    return np.linalg.inv(M)
+
+# Создание плана
+
+
+def make_plan():
+    x, p = [], []
+    grid = np.arange(-1, 1.01, 0.25)
     
-    label = Label(text="Hello METANIT.COM") # создаем текстовую метку
-    label.pack()    # размещаем метку в окне
-    
-    tabControl = ttk.Notebook(root)
-    tab1 = ttk.Frame(tabControl)
-    tab2 = ttk.Frame(tabControl)
-    tab3 = ttk.Frame(tabControl)
-    tab4 = ttk.Frame(tabControl)
-    tabControl.add(tab1, text ='Синтез')
-    tabControl.add(tab2, text ='Графики')
-    tabControl.add(tab3, text ='Таблицы')
-    tabControl.add(tab4, text ='О приложении')
-    tabControl.pack(expand = 1, fill ="both")
-    
-    
-    root.mainloop()
-    
-   
+    for i in range(len(grid)):
+        x.append([grid[i]])
+        p.append(1/n)
+
+    return x, p
+
+# Проверка оптимальности
+
+
+def A_optimal(x_plan, p_plan, delta):
+    n = 81
+    max = 0
+    grid = np.arange(-1, 1.01, 0.25)
+    x = []
+
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            x.append([grid[j], grid[i]])
+
+    for i in range(n):
+        condition = np.trace(np.dot(np.linalg.matrix_power(disp_matrix(inf_matrix(
+            x_plan, p_plan)), 2), np.dot(model(x[i], delta), np.transpose(model(x[i], delta)))))
+        if condition > max:
+            max = condition
+
+    trd = np.trace(disp_matrix(inf_matrix(x_plan, p_plan)))
+
+    return max, trd
+
+
+def plot(x_plan, p_plan):
+    fig, ax = plt.subplots()
+    cmap = plt.cm.coolwarm
+    for i in range(len(x_plan)):
+        ax.scatter(x_plan[i][0], x_plan[i][1], s=p_plan[i]*1000, cmap=cmap)
+    plt.grid()
+    plt.show()
+
+
+def main():
+    flag = True                     # Флаг выхода из градиентного спуска
+    n = 81                          # Кол-во точек на сетке
+    grad = np.zeros(n)              # Вектор градиента
+    count = 0                       # Cчётчик итераций
+    delta = 0.5
+    proj = np.zeros(n)
+
+    x_plan, p_plan = make_plan()    # Начальный план
+
+    while flag:
+        flag = False
+        M = inf_matrix(x_plan, p_plan)
+        D = disp_matrix(M)
+
+        lambdas = 0.01  # Шаг по весам
+        q = n - np.count_nonzero(p_plan)
+
+        for i in range(81):     # Вычисление градиента
+            grad[i] = np.trace(np.dot(np.linalg.matrix_power(disp_matrix(inf_matrix(
+                x_plan, p_plan, delta)), 2), np.dot(model(x_plan[i], delta), np.transpose(model(x_plan[i], delta)))))
+
+        grad /= np.linalg.norm(grad)
+
+        avg = 0.0
+        for i in range(n):
+            if p_plan[i] != 0:
+                avg += grad[i]
+        avg /= n - q
+
+        for i in range(n):
+            if p_plan[i] != 0:
+                if abs(grad[i] - avg) > 1e-10:
+                    flag = True
+
+        for j in range(0, n):
+            proj[j] = grad[j] - avg
+            if p_plan[j] == 0:
+                if proj[j] > 0:
+                    flag = True
+                else:
+                    proj[j] = 0
+
+        if count % 200 == 0:
+            print(np.trace(D))
+            # print(np.linalg.norm(proj))
+
+        if flag:
+            for i in range(0, n):
+                if proj[i] < 0 and lambdas > - p_plan[i] / proj[i]:
+                    lambdas = - p_plan[i] / proj[i]
+
+            for i in range(0, n):
+                p_plan[i] += lambdas * proj[i]
+
+        count += 1
+
+    n_plan, np_plan = [], []
+    for i in range(len(p_plan)):
+        if p_plan[i] != 0:
+            n_plan.append(x_plan[i])
+            np_plan.append((p_plan[i]))
+
+    print(count)
+    a, b = A_optimal(n_plan, np_plan, delta)
+    print(np.trace(disp_matrix(inf_matrix(n_plan, np_plan, delta))))
+
+
+if __name__ == '__main__':
+    main()
